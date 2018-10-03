a0dedaf3304f3839f5fd8da9278b0a8693e98591
Jonas Almeida <jalmeida@mathbiol.org>
2018-10-03 09:30:49 -0400

    solidifying

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+//$('#logout').hide(); <-- no longer needed
+
+// Log the user in and out on click
+const popupUri = 'popup.html';
+$('#login  button').click(() => solid.auth.popupLogin({ popupUri }));
+$('#logout button').click(() => solid.auth.logout());
+
+// Update components to match the user's login status
+solid.auth.trackSession(session => {
+  const loggedIn = !!session;
+  $('#login').toggle(!loggedIn);
+  $('#logout').toggle(loggedIn);
+  if (loggedIn) {
+    $('#user').text(session.webId);
+    // Use the user's WebID as default profile
+    if (!$('#profile').val())
+      $('#profile').val(session.webId);
+  }
+});
